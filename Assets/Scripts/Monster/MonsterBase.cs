ee497074e64dd5cbcb2f1a1d956392923d234082
meonmal <jongmin1034@naver.com>
2024-08-22 22:29:56 +0900

    0822 오후 10시

--- Assets/Scripts/Monster/MonsterBase.cs
+++ Assets/Scripts/Monster/MonsterBase.cs
@@ -7,18 +7,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.VFX;
 using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class MonsterBase : Character
 {
     public Character target;
+    public GameObject player;
     public int Lv_Hp = 200;
     public int Lv_Gold = 200;
     public float moveSpeed = 5.0f;
+    public float sponX = 30.0f;
+    public float sponY = -4.0f;
+    
+
 
     private void Start()
     {
         Init(10, 0);    // hp = 10, damage = 0
+        player = GameObject.FindGameObjectWithTag("player");
     }
 
     private void Update()
@@ -41,15 +48,20 @@ public class MonsterBase : Character
         base.OnDie();
         Debug.Log("적 사망");
         GameManager.Instance.m_Player_Value.Get_Gold(Gold, this.transform.position);
+        this.gameObject.SetActive(false);
         Spawn();
     }
 
     public void Spawn()
     {
+        
         MaxHp += MaxHp * Lv_Hp / 100;
         // MaxHp = 10 + 10 * 200 / 100 가 현재 상태다.
         Gold += Gold * Lv_Gold / 100;
         Hp = MaxHp;
         State = Character_State.Idle;
+
+        transform.position = new Vector2(sponX, sponY);
+        this.gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,13 +9,16 @@ public class Player : Character
 {
     public static Player Instance;
     public Character target;
+    public GameObject monster;
     public int Lv_Hp = 200;
     public int Lv_Gold = 200;
+    public Animator animator;
 
 
     private void Awake()
     {
         Instance = this;
+        animator = GetComponent<Animator>();
     }
 
 
@@ -29,18 +32,40 @@ public class Player : Character
         Init(10, 10);   // hp = 10, damage = 10
     }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        monster = GameObject.FindGameObjectWithTag("monster");
+    }
+
+
+
     private void Update()
     {
-        if(Attack_CoolTime < Attack_Speed)  // 어택 쿨타임이 어택 스피드보다 작을 때 실행
+        if (monster != null)
         {
-            Attack_CoolTime += Time.deltaTime;  // 어택 쿨타임은 Time.deltaTime 만큼 증가하고 
+            if (monster.GetComponent<MonsterBase>().Hp > 0)
+            {
+                animator.SetInteger("AniState", 1);
+
+                if (Attack_CoolTime < Attack_Speed)  // 어택 쿨타임이 어택 스피드보다 작을 때 실행
+                {
+                    Attack_CoolTime += Time.deltaTime;  // 어택 쿨타임은 Time.deltaTime 만큼 증가하고 
+                }
+                else        // 결국은 어택 쿨타임이 어택 스피드와 같아지게 되고 그 때 실행(공격이 실행 됨)
+                {
+                    Hit_Damage(target, Damage); // Charater의 Hit_Damage함수 실행(타겟은 유니티에서 설정, 플레이어의 데미지(지금은 10)을 실행
+                    Attack_CoolTime = 0f;   // 공격이 끝나면 어택 쿨타임은 다시 0으로 초기화 됨
+                }
+            }
         }
-        else        // 결국은 어택 쿨타임이 어택 스피드와 같아지게 되고 그 때 실행(공격이 실행 됨)
+
+        else if (monster == null)
         {
-            Hit_Damage(target, Damage); // Charater의 Hit_Damage함수 실행(타겟은 유니티에서 설정, 플레이어의 데미지(지금은 10)을 실행
-            Attack_CoolTime = 0f;   // 공격이 끝나면 어택 쿨타임은 다시 0으로 초기화 됨
+            animator.SetInteger("AniState", 2);
         }
     }
+
+    
 
     public void Spawn()
     {
@@ -56,5 +81,13 @@ public class Player : Character
         }
     }
 
+    public void Attack(BigInteger damage)
+    {
+        Get_Damage(damage);
+    }
 
+    public void Attack_Monster()
+    {
+        Attack(Damage);
+    }
 }
